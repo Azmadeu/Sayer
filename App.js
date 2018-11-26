e7323c9ccf95e9872208bb4a0de6aa2573a633e6
Azmadeu <doubleworkz@mail.ru>
2018-11-26 22:13:46 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Presentational from './src/components/presentational';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Presentational />
+      </View>
+    );
+  }
+}
